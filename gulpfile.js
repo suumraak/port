9d0e52330891921c10c29819038db9f2e5bbb3c0
Dima Vasiyev <zodiak777@inbox.ru>
2015-07-19 15:51:07 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require("gulp"),
+    browserSync = require("browser-sync"); 
+// Запуск сервера
+gulp.task('server', function() {
+	browserSync({
+		port: 9000,
+		server: {
+			baseDir: 'src'
+		}
+	});
+});
+
+// Слежка
+gulp.task('watch', function(){
+	gulp.watch([
+		'src/*.html',
+		'src/js/**/*.js',
+		'src/css/**/*.css'
+		]).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['server', 'watch']);
